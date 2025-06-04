044b596f82c4b82397f8af422f68e233f3d91cd0
Anthony Durén <anthonydadivasduren@gmail.com>
2025-06-04 08:39:07 +0200

    Added improvements to code for better readability in Bokhyllan

--- Övning Bokhyllan/Övning Bokhyllan/Program.cs
+++ Övning Bokhyllan/Övning Bokhyllan/Program.cs
@@ -4,6 +4,8 @@
  * 
  */
 
+using System.Security.Cryptography.X509Certificates;
+
 namespace Övning_Bokhyllan
 {
     class Program
@@ -25,13 +27,13 @@ namespace Övning_Bokhyllan
                 Console.WriteLine("\n\t     Välj ett alternativ: ");
 
                 // sparar användarens val
-                string val = Console.ReadLine();
+                string val = Console.ReadLine() ?? "";
 
                 // väljer funktion baserat på användarens val
                 switch (val)
                 {
                     case "1":
-                        LäggTillBok();  
+                        LäggTillBok();
                         break;
 
                     case "2":
@@ -44,7 +46,7 @@ namespace Övning_Bokhyllan
                     case "4":
                         bokLista.Clear();
                         return; // Avslutar Programmet
-                       
+
 
                     default:    // felhantering om användaren ger ett ogiltigt svar
                         ErrorMessage("\n\tOgilitgt val försök igen");
@@ -58,69 +60,43 @@ namespace Övning_Bokhyllan
         // Metod för att lägga till en ny bok i boklistan
         static void LäggTillBok()
         {
-            Console.Clear();
-            //  Frågar om författare
-            Console.WriteLine("\n\tTitel: ");
-            string titel = Console.ReadLine();
+
+            // Fråga om titel
+            string titel = FrågaAnvändare("\n\tTitel: ");
 
             // Fel hantering om titeln är tom
-            if (string.IsNullOrWhiteSpace(titel))
+            if (ErrorTomtSvar(titel, "titel"))
             {
-                ErrorMessage("\n\tDu måste ange en titel, Boken läggs inte till");
                 return; //  Avslutar Metoden tidigt
             }
 
-            Console.Clear();
+
             //Frågar efter författare
-            Console.WriteLine("\n\tFörfattare: ");
-            string författare = Console.ReadLine();
-            
+            string författare = FrågaAnvändare("\n\tFörfattare: ");
+
+
             // Fel hantering om författaren är tom
-            if (string.IsNullOrWhiteSpace(författare))
+            if (ErrorTomtSvar(författare, "författare"))
             {
-                ErrorMessage("\n\tDu måste ange en författare, Boken läggs inte till");
                 return; //  Avslutar Metoden tidigt
             }
 
-            Console.Clear();
 
             // Frågar efter utgivnings år
-            Console.WriteLine("\n\tVilket är utgivningsåret för boken");
-            // sparar input som en string
-            string input = Console.ReadLine();
+            int år = FrågaEfterÅr("\n\tVilket är utgivningsåret för boken");
+            if (år == -1) return;
 
-            //variabel för år
-            int år;
-
-            // försöker konvertera stringen till ett heltal
-            bool lyckades = int.TryParse(input, out år);
-
-            // Kontrollerar om konvertereringen misslyckades eller om året är ogiltigt
-            // ogiltiga årtal är år som är större än året vi är i nu eller negativa år 
-            // finns riktigt gamla böcker så lämnade lägsta till år 0 pga d
-            if (!lyckades || år > DateTime.Now.Year || år < 0)
-            {
-                //felmeddelande och avslutar metoden tidigt
-                ErrorMessage("\n\tOgilitgt årtal, Boken läggs inte till");
-                return;
-
-            }
-
-            Console.Clear();
             // Frågar om Bok typ
-            Console.WriteLine("\n\tTyp av bok:[1] Roman [2] Tidskrift [3] Novellsamling");
-            string typVal = Console.ReadLine();
+            string typVal = FrågaAnvändare("\n\tTyp av bok:[1] Roman [2] Tidskrift [3] Novellsamling");
 
-       
-            
 
             Bok nyBok;  // Gör en ny variabel  för nya boken
-                
+
             // skapar objektet utav boktypen användaren väljer
             switch (typVal)
             {
                 case "1":
-                    nyBok = new Roman (titel, författare, år);
+                    nyBok = new Roman(titel, författare, år);
                     break;
                 case "2":
                     nyBok = new Tidsskrift(titel, författare, år);
@@ -137,41 +113,38 @@ namespace Övning_Bokhyllan
             // Lägger till nya boken i boklistan
             bokLista.Add(nyBok);
 
-            Console.Clear ();
-            Console.WriteLine("\n\tBoken har lagts till.");
-
-            Console.WriteLine("\n\tTryck Enter för att fortsätta");
+            InfoMessage("\n\tBoken har lagts till.");
 
 
         }
 
         // Metod för att visa alla böcker i listan
         static void VisaBöcker()
-        {   
+        {
             // Om listan är tom , visa felmeddelande och återvänd till menyn
             if (bokLista.Count == 0)
             {
                 ErrorMessage("\n\tInga böcker finns i bokhyllan");
                 return;
-            } else if (bokLista.Count >= 1) 
-            {
-                Console.Clear();
-                Console.WriteLine("\n\tLista över bokhyllans böcker: ");
-
-                // loopar igenom alla böker och använder VisaInfo metoden som skriver ut Bok info
-                foreach (Bok bok in bokLista)
-                {
-                    bok.VisaInfo();
-                    Console.WriteLine("\n"); 
-                }
             }
+
+            Console.Clear();
+            Console.WriteLine("\n\tLista över bokhyllans böcker: ");
+
+            // loopar igenom alla böker och använder VisaInfo metoden som skriver ut Bok info
+            foreach (Bok bok in bokLista)
+            {
+                bok.VisaInfo();
+                Console.WriteLine("\n");
+            }
+
 
             // Pausar Programmet för låta anvädaren hinna se böckerna innan programmet återvänder till menyn
             Console.WriteLine("\n\tTryck Enter för att gå tillbaka till menyn");
             Console.ReadLine();
 
         }
-        
+
         // Metod för Felmeddelanden 
         static void ErrorMessage(string msg)
         {
@@ -179,28 +152,94 @@ namespace Övning_Bokhyllan
             Console.WriteLine(msg);
             Console.WriteLine("\n\tTryck Enter för att fortsätta");
             Console.ReadLine();
-            
+
+        }
+
+        static void InfoMessage(string msg)
+        {
+            Console.Clear();
+            Console.WriteLine(msg);
+            Console.WriteLine("\n\tTryck Enter för att fortsätta");
+            Console.ReadLine();
+
         }
 
         // Metod för att rensa boklistan
         static void Rensalistan()
         {
-            Console.Clear(); 
+            Console.Clear();
 
             if (bokLista.Count > 0)
             {
-                bokLista.Clear(); // rensar boklistan
-                Console.WriteLine("\n\tBokhyllan har rensats");
-                Console.WriteLine("\n\tTryck enter för att fortsätta");
-                Console.ReadLine();
-            }else // felhantering om boklistan är tom 
+                string svar = FrågaAnvändare("\n\tÄr du säker på att du vill rensa bokhyllan? (Ja/Nej)");
+                if (svar.ToLower() == "ja")
+                {
+                    bokLista.Clear(); // rensar boklistan
+                    InfoMessage("\n\tBokhyllan har rensats");
+                }
+                else if (svar.ToLower() == "nej")
+                {
+                    
+                    InfoMessage("\n\tRensning avbruten");
+                    
+                }else
+                {
+                    ErrorMessage("\n\tOgiltigt svar rensning avbruten");
+                }
+                
+            }
+            else // felhantering om boklistan är tom 
             {
                 ErrorMessage("\n\tBokhyllan är redan tom");
             }
-            
+
         }
 
-            
+        static string FrågaAnvändare(string fråga)
+        {
 
+            Console.Clear();
+            Console.WriteLine($"\n\t{fråga}");
+
+
+            return Console.ReadLine() ?? "";
+
+        }
+
+        static bool ErrorTomtSvar(string svar, string fältnamn)
+        {
+
+            if (string.IsNullOrWhiteSpace(svar))
+            {
+                ErrorMessage($"\n\tDu måste ange en {fältnamn}, Boken läggs inte till");
+                return true;
+            }
+            return false;
+        }
+
+        static int FrågaEfterÅr(string fråga)
+        {
+
+            // Frågar efter utgivnings år
+            // och sparar input som en string
+            string input = FrågaAnvändare(fråga);
+            //variabel för år
+            int år;
+
+            // försöker konvertera stringen till ett heltal
+            bool lyckades = int.TryParse(input, out år);
+
+            // Kontrollerar om konvertereringen misslyckades eller om året är ogiltigt
+            // ogiltiga årtal är år som är större än året vi är i nu eller negativa år 
+            // finns riktigt gamla böcker så lämnade lägsta till år 0 pga d
+
+            if (!lyckades || år > DateTime.Now.Year || år < 0)
+            {
+                ErrorMessage("\n\tOgiltigt årtal, Boken läggs inte till");
+                return -1; // signalerar ogiltigt år
+            }
+
+            return år;
+        }
     }
 }
